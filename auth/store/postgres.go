fac52a9521b8ecaf745b4ac3328dccf12f055ffd
KentoNion <mrkentonion@gmail.com>
2024-12-01 16:19:04 +0300

    db tweaks, migrations added

--- auth/store/postgres.go
+++ auth/store/postgres.go
@@ -2,17 +2,29 @@ package Store
 
 import (
 	"context"
-	"database/sql"
+	sq "github.com/Masterminds/squirrel"
+	"github.com/bool64/sqluct"
+	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
 )
 
 type Store struct {
-	conn *sql.DB
+	db *sqlx.DB
+	sq sq.StatementBuilderType
+	sm sqluct.Mapper
+}
+
+func NewDB(db *sqlx.DB) *Store {
+	return &Store{
+		db: db,
+		sq: sq.StatementBuilder.PlaceholderFormat(sq.Dollar),
+		sm: sqluct.Mapper{Dialect: sqluct.DialectPostgres},
+	}
 }
 
 func (s Store) Save(ctx context.Context, token string, userID string) error {
 	query := "INSERT INTO tokens (user_id, token) VALUES (?, ?) ON CONFLICT DO UPDATE token = ?"
-	_, err := s.conn.ExecContext(ctx, query, userID, token, token)
+	_, err := s.db.ExecContext(ctx, query, userID, token, token)
 	if err != nil {
 		return errors.Wrap(err, "failed to save token")
 	}
@@ -21,7 +33,7 @@ func (s Store) Save(ctx context.Context, token string, userID string) error {
 
 func (s Store) Get(ctx context.Context, token string) (bool, error) {
 	query := "SELECT 1 FROM tokens where token = ?"
-	rows, err := s.conn.QueryContext(ctx, query, token)
+	rows, err := s.db.QueryContext(ctx, query, token)
 	if err != nil {
 		return false, errors.Wrap(err, "failed to query")
 	}
---
+++ migrations/migrationsUp.go
@@ -0,0 +1,27 @@
+package migrations
+
+import (
+	"github.com/pkg/errors"
+	"os"
+	"os/exec"
+)
+
+func RunGooseMigrations(dbname string) error {
+	// Создаем команду для запуска goose
+	cmd := exec.Command(
+		"goose",
+		"-dir", "./migrations",
+		"up",
+	)
+	// Устанавливаем переменные окружения
+	cmd.Env = append(os.Environ(),
+		"GOOSE_DRIVER=postgres", "GOOSE_DBSTRING=host=localhost user=postgres password=postgres database=medodsTest sslmode=")
+
+	// Выполняем команду и захватываем вывод
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		return errors.Wrap(err, "Error running goose migrations")
+	}
+
+	return nil
+}
